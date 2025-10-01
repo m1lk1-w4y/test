9a37db136142bb3f776d3762f14355990296e155
m1lk1-w4y <cafzu541@gmail.com>
2025-10-01 15:27:13 -0500

    add hours on list messages

--- src/app/admin/messages/page.tsx
+++ src/app/admin/messages/page.tsx
@@ -36,7 +36,7 @@ const ProjectsSection = () => {
           <div className="bg-[#181818]  relative my-3 mx-3 py-3 px-3">
             <div className="grid grid-cols-6 sm:grid-cols-12">
               <div className="col-span-12 sm:text-left justify-self-start">
-                <h3 className="text-white py-2" key={r._id}><span className="font-bold">PUBLISHED ON:</span><DateFormatter dateString={r.createdAt} /></h3>
+                <h3 className="text-white py-2" key={r._id}><span className="font-bold">PUBLISHED ON: </span><DateFormatter dateString={r.createdAt} /></h3>
                 <h3 className="text-white py-2" key={r._id}><span className="font-bold">ID:</span>{` ${r._id}`}</h3>
                 <h3 className="text-white py-2" key={r._id}><span className="font-bold">Email:</span>{` ${r.email}`}</h3>
                 <h3 className="text-white py-2" key={r._id}><span className="font-bold">Subject:</span>{` ${r.subject}`}</h3>
