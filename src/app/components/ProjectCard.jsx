4029a5b196ba1013d53f7bf369aebbbe1b9ebac3
m1lk1-w4y <cafzu541@gmail.com>
2025-10-03 00:23:46 -0500

    next link

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Chip from '@mui/material/Chip';
+import Link from 'next/link'
 
 const ProjectCard = ({ image, title, description, github, web, tag2 }) => {
 
@@ -11,20 +12,22 @@ const ProjectCard = ({ image, title, description, github, web, tag2 }) => {
         style={{ background: `url(${image})`, backgroundSize: "cover" }}
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
-          <a
+          <Link
             href={github}
+            replace
             target="_blank"
             className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
             <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
-          </a>
-          <a
+          </Link>
+          <Link
             href={web}
+            replace
             target="_blank"
             className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
             <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
-          </a>
+          </Link>
         </div>
       </div>
       <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
