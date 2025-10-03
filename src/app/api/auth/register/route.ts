d5bc6bf1cde05615e76f466dd4cdeab06f89a033
m1lk1-w4y <cafzu541@gmail.com>
2025-10-03 16:25:43 -0500

    first commit

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -11,7 +11,7 @@ db.connect()
 export async function POST(request: NextRequest) {
  try {
   const reqBody = await request.json()
-  const { username, email, password } = reqBody
+  const { name, email, password } = reqBody
 
   console.log(reqBody);
 
@@ -27,7 +27,7 @@ export async function POST(request: NextRequest) {
   const hashedPassword = await bcryptjs.hash(password, salt)
 
   const newUser = new User({
-   username,
+   name,
    email,
    password: hashedPassword
   })
